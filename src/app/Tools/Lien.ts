22359070823453e72db59580f9c6bcdfc0a00dbd
unknown <grace1mihigo@gmail.com>
2025-08-30 14:34:16 +0200

    lien

--- src/app/Tools/Lien.ts
+++ src/app/Tools/Lien.ts
@@ -1,4 +1,5 @@
-const localhost = "http://localhost:4000";
+//const localhost = "http://localhost:4000";
+const localhost = "https://ivannebackend.onrender.com";
 //const localhost = "https://parma.bboxxvm.com";
 //const localhost = "https://backendpharmacie.onrender.com";
 export const lien = `${localhost}/pharmacie`;
